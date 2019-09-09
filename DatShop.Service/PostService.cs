154aa2e6f976b8913337354e06888eaa155fc6e5
ltdat2010 <43429510+ltdat2010@users.noreply.github.com>
2019-09-09 15:40:28 +0700

    Update 20190909

---
+++ DatShop.Service/PostService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatShop.Data.Infrastructure;
+using DatShop.Data.Repository;
+using DatShop.Model.Models;
+
+
+namespace DatShop.Service
+{
+    public interface IPostService
+    {
+        void Add(Post post);
+        void Update(Post post);
+        void Delete(int id);
+        IEnumerable<Post> GetAll();
+        IEnumerable<Post> GetAllPaging(int page, int pageSize, out int totalRow);
+        Post GetById(int id);
+        IEnumerable<Post> GetAllTagPaging(int page, int pageSize, out int totalRow);
+        void SaveChanges();
+
+    }
+    public class PostService : IPostService
+    {
+        IPostRepository _iPostRepository;
+        IUnitOfWork _iUnitOfWork;
+
+        public PostService(IPostRepository iPostRepsitory, IUnitOfWork iUnitOfWork)
+        {
+            this._iPostRepository = iPostRepsitory;
+            this._iUnitOfWork = iUnitOfWork;
+        }
+        public void Add(Post post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Post> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Post> GetAllPaging(int page, int pageSize, out int totalRow)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Post> GetAllTagPaging(int page, int pageSize, out int totalRow)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Post GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Post post)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
